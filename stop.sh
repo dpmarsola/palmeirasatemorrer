c4d643a642acb6c3c6780f95220332d1e1009c2e
Ubuntu <ubuntu@ip-172-31-33-226.ec2.internal>
2024-05-08 21:28:22 -0300

    adjustments

--- stop.sh
+++ stop.sh
@@ -12,6 +12,7 @@ echo $(pgrep -f runserver) >> ./logs/stop.log
 echo $(pgrep -f jobSendMessage) >> ./logs/stop.log
 echo $(pgrep -f sendMessage) >> ./logs/stop.log
 echo $(pgrep -f pywhatkitWrapper) >> ./logs/stop.log
+echo $(pgrep -f chrome) >> ./logs/stop.log
 
 echo $(date) ">>>> Killing them now...." >> ./logs/stop.log
 kill -9 $(pgrep -f ngrok) >> ./logs/stop.log 2>&1
@@ -19,5 +20,6 @@ kill -9 $(pgrep -f runserver) >> ./logs/stop.log 2>&1
 kill -9 $(pgrep -f jobSendMessage) >> ./logs/stop.log 2>&1
 kill -9 $(pgrep -f sendMessage) >> ./logs/stop.log 2>&1
 kill -9 $(pgrep -f pywhatkitWrapper) >> ./logs/stop.log 2>&1
+kill -9 $(pgrep -f chrome) >> ./logs/stop.log 2>&1
 
 echo $(date) ">>>>>>>>>> All processes killed <<<<<<<<<<" >> ./logs/stop.log
--- unstuck.sh
+++ unstuck.sh
@@ -7,11 +7,13 @@ echo $(date) ">>>>>>>>>> Killing stuck processes <<<<<<<<<<" >> ./logs/unstuck.l
 echo $(date) ">>>> The following processes might be stuck:" >> ./logs/unstuck.log
 echo $(pgrep -f jobSendMessage) >> ./logs/unstuck.log
 echo $(pgrep -f sendMessage) >> ./logs/unstuck.log
+echo $(pgrep -f chrome) >> ./logs/unstuck.log
 
 echo $(date) ">>>> Killing them now...." >> ./logs/unstuck.log
 
 kill -9 $(pgrep -f jobSendMessage) >> ./logs/unstuck.log 2>&1
 kill -9 $(pgrep -f sendMessage) >> ./logs/unstuck.log 2>&1
+kill -9 $(pgrep -f chrome) >> ./logs/unstuck.log 2>&1
 
 echo $(date) ">>>> Moving stuck files to ./stuck" >> ./logs/unstuck.log
 mv ./tosend/*.msg ./stuck 2> /dev/null 
