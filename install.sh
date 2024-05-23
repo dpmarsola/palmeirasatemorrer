23d9e9c2e068f9cdafe57dd9a1b6aecce371dae5
Ubuntu <ubuntu@ip-172-31-28-19.ec2.internal>
2024-05-22 22:05:17 -0300

    setting installation file at full

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/bash
 
+codename=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d'=' -f2)
 
-sudo apt install python3-tk python3-dev
+echo "====== Setting packages for download ======"
+echo "
+deb [arch=arm64] http://ports.ubuntu.com/ <<version_codename>> main multiverse universe
+deb [arch=arm64] http://ports.ubuntu.com/ <<version_codename>>-security main multiverse universe
+deb [arch=arm64] http://ports.ubuntu.com/ <<version_codename>>-backports main multiverse universe
+deb [arch=arm64] http://ports.ubuntu.com/ <<version_codename>>-updates main multiverse universe
+" > tmp
+
+sed -i 's/<<version_codename>>/'${codename}'/g' tmp
+
+sudo mv tmp /etc/apt/sources.list.d/my_list.list
+
+echo "====== Creating directories ==============="
+mkdir ./logs ./sent ./tosend ./security ./stuck
+
+echo "====== Installing Python and Its Apps ====="
+sudo apt install python3-tk -y
 
 if [ $? -ne 0 ]
 then
@@ -9,7 +26,7 @@ then
     exit 1
 fi
 
-sudo apt install python3-dev
+sudo apt install python3-dev -y
 
 if [ $? -ne 0 ]
 then
@@ -17,17 +34,15 @@ then
     exit 1
 fi
 
-sudo snap install ngrok
+sudo apt install python3-pip -y
 
 if [ $? -ne 0 ]
 then
-    echo "Failed to install ngrok"
+    echo "Failed to install python3-pip"
     exit 1
 fi
 
-mkdir ./logs ./sent ./tosend ./security ./stuck
-
-pip install django
+pip install django --break-system-packages
 
 if [ $? -ne 0 ]
 then
@@ -35,7 +50,7 @@ then
     exit 1
 fi
 
-pip install pywhatkit
+pip install pywhatkit --break-system-packages
 
 if [ $? -ne 0 ]
 then
@@ -43,25 +58,71 @@ then
     exit 1
 fi
 
-ngrok config add-authtoken <check your ngrok account for the token>
+echo "====== Installing NGROK Agent ============="
+sudo snap install ngrok 
 
 if [ $? -ne 0 ]
 then
-    echo "Failed to add authtoken to ngrok"
+    echo "Failed to install ngrok"
     exit 1
 fi
 
+echo "Please enter your NGROK token here <check your ngrok account for the token>: "
+read token
 
-echo "email" >> ./security/credentials
-echo "password" >> ./security/credentials
+if [ ! -z $token ]
+then
+    ngrok config add-authtoken $token
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to add authtoken to ngrok"
+        exit 1
+    fi
+else
+    echo "No token was provided."
+    exit 1
+fi
 
+echo "====== Setting BREVO credentials =========="
+echo "Please enter your email and password for access BREVO service: "
+read -p "EMAIL: " email
+read -s -p "PASSWORD: " password
+echo $email >> ./security/credentials
+echo $password >> ./security/credentials
 
+echo "====== Installing X11 and Its Stuff ======="
+sudo apt-get install xorg openbox -y
+
+if [ $? -ne 0 ]
+then
+    echo "Failed to install x11 and openbox"
+    exit 1
+fi
+
+echo "====== Installing XFCE ===================="
+sudo apt-get install xfce4-session xfce4-goodies -y
+
+if [ $? -ne 0 ]
+then
+    echo "Failed to install xfce"
+    exit 1
+fi
+
+echo "====== Set Service to Run at Startup ======"
 sudo cp ./cfg/palmeiras.service /etc/systemd/system
 sudo chmod 644 /etc/systemd/system/palmeiras.service
 sudo systemctl enable palmeiras.service
 sudo systemctl daemon-reload
 
-echo "Please rebot your system..."
+echo "====== Finalizing Installation ============"
+read -p "System need to reebot. Want to reboot it now (Y/N)?" $reboot
+
+if [ "$reboot" = "Y" ]
+then
+    sudo shutdown -r now
+else
+    exit 0
+fi
 
 #todo list
 # make tigerVNC server run at startup
